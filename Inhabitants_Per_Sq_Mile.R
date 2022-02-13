f938088412e584a5c7c4672d0f3b90df6fa662fb
Connor McNamee <mcnameco@mail.gvsu.edu>
2022-02-12 21:38:29 -0500

    Finishing Pop. Density graph

--- Inhabitants_Per_Sq_Mile.R
+++ Inhabitants_Per_Sq_Mile.R
@@ -2,7 +2,12 @@ library(tidyverse)
 library(showtext)
 font_add_google("Puritan", "Puritan")
 showtext_auto()
-Total_Population <- read_csv("Total_Population.csv")
+
+Censuses <- c("1790", "1800", "1810", "1820", "1830", "1840", "1850", "1860", "1870", "1880", "1890")
+Population <- c(3929214,5308483,7239881,9633822,12866020,17069453,23191876,31443321,38558371,50155783,62622250)
+Density <- c(4.75,6.41,3.62,4.82,6.25,8.29,7.78,10.39,12.74,16.58,20.70)
+Urban_Pop <- c(119748,201806,315653,467252,856512,1449569,2896539,4645000,7538000,10794000,17446000)
+Population_Density <- tibble(Censuses,Population,Density,Urban_Pop)
 
 gannett <- function() {
   theme(axis.title = element_text(size = 9, family = "serif"),
@@ -17,14 +22,14 @@ gannett <- function() {
         panel.border = element_rect(fill = NA, size = 1.5)
   )
 }
-Mod_Total_Population <- Total_Population %>% 
+Mod_Total_Population <- Population_Density %>% 
   mutate(New_censuses = as.character(Censuses)) %>% 
-  select(New_censuses, `Per sq. Mile`)
+  select(New_censuses, Density)
 
 ggplot(Mod_Total_Population) +
   annotate("segment", x = seq(0,20,2), xend = seq(0,20,2), y = 0, yend = 11.38) +
   annotate("segment", x = -2, xend = 22, y = 11.38, yend = 11.38) +
-  geom_col(aes(y = New_censuses, x = `Per sq. Mile`), width = .4, color = "#1a1e19", fill = "#100e05") +
+  geom_col(aes(y = New_censuses, x = Density), width = .4, color = "#1a1e19", fill = "#100e05") +
   labs(y = NULL, x = NULL, title = "Number of Inhabitants to the Square Mile \nat Each Census: 1790 to 1890") +
   scale_y_discrete(limits = rev) +
   scale_x_continuous(breaks = seq(0,20,2), position = "top", limit = c(-2,22)) +

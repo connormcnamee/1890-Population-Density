f49046d20ea7423b78a1abb32617799be00f50f4
Connor McNamee <mcnameco@mail.gvsu.edu>
2022-04-02 12:09:11 -0400

    adding more extended graphs

--- Inhabitants_Per_Sq_Mile.R
+++ Inhabitants_Per_Sq_Mile.R
@@ -51,3 +51,40 @@ ggplot(Mod_Total_Population) +
         panel.border = element_rect(fill = NA, size = 1.05)
   )
 dev.off()
+
+
+Ext_Censuses <- c("1790", "1800", "1810", "1820", "1830", "1840", "1850", "1860", "1870", "1880", "1890", "1900","1910","1920","1930")
+Ext_Density <- c(4.75,6.41,3.62,4.82,6.25,8.29,7.78,10.39,12.74,16.58,20.70,25.6,30.9,35.5,41.3)
+Ext_Pop_Density <- tibble(Ext_Censuses,Ext_Density)
+Ext_Density <- Ext_Pop_Density %>% 
+  mutate(New_censuses = as.character(Ext_Censuses)) %>% 
+  select(New_censuses, Ext_Density)
+
+# Extended Graph
+
+png("Ext-Pop-Density.png",width=500,height=575)
+ggplot(Ext_Density) +
+  annotate("segment", x = seq(0,40,4), xend = seq(0,40,4), y = 0, yend = 15.4) +
+  annotate("segment", x = -4, xend = 44, y = 15.4, yend = 15.4) +
+  geom_col(aes(y = New_censuses, x = Ext_Density), width = .4, color = "#1a1e19", fill = "#100e05") +
+  labs(y = NULL, x = NULL, title = "Number of Inhabitants to the Square Mile \nat Each Census: 1790 to 1930") +
+  scale_y_discrete(limits = rev) +
+  scale_x_continuous(breaks = seq(0,40,4), position = "top", limit = c(-4,44)) +
+  theme_linedraw() +
+  coord_cartesian(xlim = c(-.9,42), ylim = c(1, 15.22)) +
+  theme(axis.title = element_text(size = 9, family = "serif"),
+        axis.text = element_text(size = 7.5, family = "serif"),
+        plot.title = element_text(size = 13, family = "Puritan", hjust = .5, vjust = -2),
+        axis.title.y = element_text(hjust = .5),
+        axis.ticks = element_blank(),
+        axis.text.y = element_text(size = 7.5, vjust = .75, margin = margin(r = -25.5)),
+        axis.text.x.top = element_text(margin = margin(b = -15.5, t = 14)),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        aspect.ratio = 1.2,
+        panel.border = element_rect(fill = NA, size = 1.05)
+  )
+dev.off()
+
--- Urban_Population.R
+++ Urban_Population.R
@@ -71,7 +71,7 @@ ggplot(Urban_Pop_Graph) +
   ) 
 dev.off()
 
-
+# Creating graph extending through 1910
 png("Ext-Urban-Pop.png",width=900,height=500)
 ggplot(Ext_Pop_Graph) +
   # Total population
@@ -84,6 +84,46 @@ ggplot(Ext_Pop_Graph) +
   # Add box to go around the census years
   coord_cartesian(xlim = c(.4, 92)) +
   scale_x_continuous(breaks = seq(0,90, by = 10), position = "top", limits = c(0,92)) +
+  theme_linedraw() +
+  theme(axis.title = element_text(size = 6, family = "serif"),
+        plot.title = element_text(size = 8, family = "Puritan", hjust = .5, vjust = -10),
+        plot.subtitle = element_text(size = 8.5, family = "Puritan", vjust = -119, hjust = .001),
+        axis.title.y = element_text(hjust = .5),
+        axis.text.y = element_text(size = 7, margin = margin(l = 20, r = -29)),
+        axis.text.x = element_text(size = 6),
+        axis.ticks = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.y = element_blank(),
+        # Make the graph wider instead of taller
+        aspect.ratio = .3,
+        panel.border = element_rect(fill = NA, size = 1.25)
+  ) 
+dev.off()
+
+year <- c("1910", "1920", "1930", "1940")
+total <- c(91972266,105710620,122775046,131669275)
+urban <- c(42166120,54304603,68954823,74423702)
+
+New_Urban_Pop <- tibble(year,total,urban)
+
+New_Pop_Graph <- New_Urban_Pop %>% 
+  mutate(Urban_Pop = urban / 1000000,
+         Censuses = as.character(year),
+         Population = total / 1000000) %>% 
+  select(Urban_Pop, Censuses, Population)
+
+png("Urban-1910-1940.png",width=1000,height=500)
+ggplot(New_Pop_Graph) +
+  # Total population
+  geom_col(aes(y = Censuses, x = Population), width = .6, color = "black", fill = "white", alpha = 0) +
+  # Urban Population overlaid on total population
+  geom_col(aes(y = Censuses, x = Urban_Pop), width = .6, fill = "black") +
+  labs(x = "[Millions of Inhabitants]", y = "", title = "Urban and Total Population at Each Census: 1910 to 1940",
+       subtitle = "   The total length of each bar represents the aggregate population, while the black portion of each bar indicates the urban element--that is, the population contained in cities \nhaving 2500 inhabitants or more.") +
+  scale_y_discrete(limits = rev) +
+  # Add box to go around the census years
+  coord_cartesian(xlim = c(.4, 250)) +
+  scale_x_continuous(breaks = seq(0,60, by = 10), position = "top", limits = c(0,70)) +
   theme_linedraw() +
   theme(axis.title = element_text(size = 6, family = "serif"),
         plot.title = element_text(size = 8, family = "Puritan", hjust = .5, vjust = -10),
